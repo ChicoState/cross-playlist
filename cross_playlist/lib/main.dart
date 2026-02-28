1f7a77727f69a826e025233ec78803ec3336df9d
Thomas Orozco <torozco2@csuchico.edu>
2026-02-28 02:15:01 -0800

    Started a simple user interface, needs more work ie delete song from playlist, connecting to streaming services

--- cross_playlist/lib/main.dart
+++ cross_playlist/lib/main.dart
@@ -15,6 +15,32 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cross-Playlist',
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          if (snapshot.hasData) {
+            return const MyHomePage();
+          }
+          return const LoginPage();
+        },
+      ),
+    );
+  }
+}
+
+
+class MyHomePage  extends StatelessWidget {
+  const MyHomePage ({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
