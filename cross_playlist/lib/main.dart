e8c6a5fa77af37f4cdca9030183ffdc06198db79
Thomas Orozco <torozco2@csuchico.edu>
2026-02-28 01:49:22 -0800

    Sarted a simple user interface, needs more work ie delete song from playlist, connecting to streaming services

--- cross_playlist/lib/main.dart
+++ cross_playlist/lib/main.dart
@@ -19,125 +19,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-          if (snapshot.hasData) {
-            return const MyHomePage(title: 'Cross Playlist');
-          }
-          return const LoginPage();
-        },
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text('Cross-Playlist', style: TextStyle(fontSize: 36))
+        ),
+      body: const Playlist(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class Song {
+  const Song(this.artist, this.album, this.genre, this.streamingPlatform, {required this.name, required this.url});
+  
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  final String name, url, artist, album, genre, streamingPlatform;
+    
+  // Connects music streaming service. NEEDS IMPLEMENTATION
+  void _stream(String command) {
+    if (command == 'play') {
+      print("Play"); // Delete, Only used for testing buttons. Prints to consol only, not App
+    } else if (command == 'stop') {
+      print("Stop"); // Delete, Only used for testing buttons. Prints to consol only, not App
+    }
+    
+  }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// Refreshes Playlist state on adding a song to the playlist
+class Playlist extends StatefulWidget {
+  const Playlist({super.key});
 
-  void _incrementCounter() {
+  @override
+  State<Playlist> createState() => _PlaylistState();
+}
+
+
+class _PlaylistState extends State<Playlist> {
+  
+  final List<Song> songList = List<Song>.generate(2, (int index) => Song("", "", "", "", name: "", url: "") ,growable: true);
+  void _addSong() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // String artist = "", album = "", genre = "", streamingPlatform = "", name = "", url = "";
+      String artist = 'artist', album = 'album', genre = '', streamingPlatform = '', name = 'Test Name', url = 'test.com';
+      songList.add(Song(artist, album, genre, streamingPlatform, name: name, url: url));
     });
   }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return ReorderableListView(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      onReorder: (int oldIndex, int newIndex){
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final Song switchedSong = songList.removeAt(oldIndex);
+          songList.insert(newIndex, switchedSong);
+        });
+      },
+      footer: FloatingActionButton(
+        onPressed: _addSong,
+        tooltip: "Adds song to playlist",
+        child: Icon(Icons.add),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
+      children: <Widget> [
+        //Loops the number of songs on the playlist to display in Reorderable List View
+        for(int index = 0; index < songList.length; index += 1)
+          ListTile(
+            shape: Border.all(width: 3, color: Colors.white),
+            key: Key('$index'),
+            tileColor: Colors.lightBlue,
+            contentPadding: EdgeInsets.symmetric(vertical: 30, horizontal: 80),
+            leading: Container(
+              width: 100,
+              height: 150,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2)
+              ),
+              child: Center(
+                child: Text("Album\nCover"),
+              ),
+            ),      
+            title: Container(
+              width: 80,
+              height: 90,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2)
+              ),
+              child: Center(
+                child: Text('songList[index].name \nsongList[index].artist \nsongList[index].album'),
+              ),
+            ),
+            trailing: SizedBox(
+              width: 112,
+              height: 80,
+              child: Row(
+                children: [
+                  FloatingActionButton(
+                  onPressed: () => songList[index]._stream('play'),
+                  tooltip: "Plays/Pauses music",
+                  child: const Icon(Icons.play_arrow, size: 40, color: Colors.green)),
+                  FloatingActionButton(
+                  onPressed: () => songList[index]._stream('stop'),
+                  tooltip: "Stops music",
+                  child: const Icon(Icons.stop, size: 40, color: Colors.red,),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
     );
   }
 }
+
