d73f9622494746b03bf7bb34b4f4433602211b1f
Thomas Orozco <torozco2@csuchico.edu>
2026-03-01 18:55:49 -0800

    Added comments for various lines and what needs to be implemented

--- cross_playlist/lib/main.dart
+++ cross_playlist/lib/main.dart
@@ -5,13 +5,15 @@ import 'firebase_options.dart';
 import 'login_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); //Firebase initialization
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
 
+// Handles the login page from Firebase to get into the homepage
+// of the app
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
               body: Center(child: CircularProgressIndicator()),
             );
           }
-          if (snapshot.hasData) {
+          if (snapshot.hasData) {  //Goes to Home page on sucessful login 
             return const MyHomePage();
           }
           return const LoginPage();
@@ -37,11 +39,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
+// Displays the playlist on the homepage after successful login
 class MyHomePage  extends StatelessWidget {
   const MyHomePage ({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -55,15 +56,17 @@ class MyHomePage  extends StatelessWidget {
   }
 }
 
-
+// Song class that will handle the playing of music
 class Song {
-  const Song(this.artist, this.album, this.genre, this.streamingPlatform, {required this.name, required this.url});
+  const Song(this.artist, this.album, this.genre, this.streamingPlatform, 
+             {required this.name, required this.url});
   
-
   final String name, url, artist, album, genre, streamingPlatform;
     
   // Connects music streaming service. NEEDS IMPLEMENTATION
   void _stream(String command) {
+    // This will handle the logic when the user press the Play/Pause and Stop buttons
+    // Below is some testing code to make sure the buttons were working correctly.
     if (command == 'play') {
       print("Play"); // Delete, Only used for testing buttons. Prints to consol only, not App
     } else if (command == 'stop') {
@@ -82,17 +85,28 @@ class Playlist extends StatefulWidget {
   State<Playlist> createState() => _PlaylistState();
 }
 
+// Creates the user's playlist and allow easy rearranging the order of the list
+// ***Note: Things to work on****
+//    1. a way to take user input for the song information 
+//    2. a way to save the playlist so it doesn't get deleted every time the app boots
+//       also saving multiple playlist with a custom name
+//    3. a way to keep track of what song is playing to auto play the next song
+//       since every song has a play button, multiple songs should not be able to play
+//       over eachother.
+//    4. NEEDS a delete song/s option 
 
 class _PlaylistState extends State<Playlist> {
   
   final List<Song> songList = List<Song>.generate(2, (int index) => Song("", "", "", "", name: "", url: "") ,growable: true);
   void _addSong() {
     setState(() {
-      // String artist = "", album = "", genre = "", streamingPlatform = "", name = "", url = "";
+      // Need to add user input to fill the song information
       String artist = 'artist', album = 'album', genre = '', streamingPlatform = '', name = 'Test Name', url = 'test.com';
       songList.add(Song(artist, album, genre, streamingPlatform, name: name, url: url));
     });
   }
+
+  // Builds the song list into a scrollable and reorderable playlist
   @override
   Widget build(BuildContext context) {
     return ReorderableListView(
@@ -140,11 +154,13 @@ class _PlaylistState extends State<Playlist> {
                 child: Text('songList[index].name \nsongList[index].artist \nsongList[index].album'),
               ),
             ),
+
+            // Addes Play/Pause and Stop bottons. When pressed, they call stream() function from the song class
             trailing: SizedBox(
               width: 112,
               height: 80,
               child: Row(
-                children: [
+                children: [ 
                   FloatingActionButton(
                   onPressed: () => songList[index]._stream('play'),
                   tooltip: "Plays/Pauses music",
